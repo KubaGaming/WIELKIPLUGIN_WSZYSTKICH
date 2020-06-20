53e362b171e5a549ddad3d3a3c6a3514bcb4d5b6
KubaGaming <52156486+KubaGaming@users.noreply.github.com>
2020-06-20 14:26:45 +0200

    Create Lang.java

---
+++ enums/Lang.java
@@ -0,0 +1,45 @@
+package WIELKIPLUGIN.enums;
+ 
+import org.bukkit.ChatColor;
+import org.bukkit.configuration.file.YamlConfiguration;
+ 
+public enum Lang {
+    TITLE("plugin-title", "&4WielkiPlugin:"),
+    HELPCMD("help-command", "/** dodajcie własną wiadomość *\"),
+    INVALID_ARGS("invalid-args", "&cBłąd, sprawdź czy podałeś odpowiednią ilość argumentów!"),
+    PLAYER_ONLY("player-only", "Przepraszamy konsolo!"),
+    NO_PERMS("no-permissions", "&cNie posiadasz uprawnien do tej komendy");
+ 
+    private String path;
+    private String def;
+    private static YamlConfiguration LANG;
+ 
+
+    Lang(String path, String start) {
+        this.path = path;
+        this.def = start;
+    }
+ 
+
+    public static void setFile(YamlConfiguration config) {
+        LANG = config;
+    }
+ 
+    @Override
+    public String toString() {
+        if (this == TITLE)
+            return ChatColor.translateAlternateColorCodes('&', LANG.getString(this.path, def)) + " ";
+        return ChatColor.translateAlternateColorCodes('&', LANG.getString(this.path, def));
+    }
+ 
+
+    public String getDefault() {
+        return this.def;
+    }
+ 
+
+    public String getPath() {
+        return this.path;
+    }
+}
+ 
